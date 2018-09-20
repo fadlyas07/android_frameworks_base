ad5e9ee374cb396d8e9dbb1e3e31b61f0dc88d36
romanbb <romanbirg@gmail.com>
2018-09-20 23:22:51 +0530

    SystemUI: QS: Add Compass tile
    - Port to 11, 12 - idoybh (YAAP)
    - Port to 10 - Stallix
    - Port to Pie - SuperDroidBond
    - Port to Oreo - LorDClockAN
    
    -----
    
    SystemUI: CompassTile: Set default state inactive
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I259c251ff4b9740a09c4d31d770f82549bff1eae
    
    -----
    
    SystemUI: CompassTile: Do not use slashed icon
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Change-Id: I762f8e486cb9bc908d0307a66de2d1d667406c1c
    
    -----
    
    SystemUI: CompassTile: Disable long click
    
    Change-Id: I86c784644e83cba2a4eacc0b9adb9abf1a334a2b

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -37,6 +37,7 @@ import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorCorrectionTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
+import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DeviceControlsTile;
 import com.android.systemui.qs.tiles.DndTile;
@@ -102,6 +103,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<PowerShareTile> mPowerShareTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
     private final Provider<DataSwitchTile> mDataSwitchTileProvider;
+    private final Provider<CompassTile> mCompassTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -141,7 +143,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<ColorCorrectionTile> colorCorrectionTileProvider,
             Provider<PowerShareTile> powerShareTileProvider,
             Provider<CaffeineTile> caffeineTileProvider,
-            Provider<DataSwitchTile> dataSwitchTileProvider) {
+            Provider<DataSwitchTile> dataSwitchTileProvider,
+            Provider<CompassTile> compassTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -177,6 +180,7 @@ public class QSFactoryImpl implements QSFactory {
         mPowerShareTileProvider = powerShareTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
         mDataSwitchTileProvider = dataSwitchTileProvider;
+        mCompassTileProvider = compassTileProvider;
     }
 
     /** Creates a tile with a type based on {@code tileSpec} */
@@ -257,6 +261,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mCaffeineTileProvider.get();
             case "dataswitch":
                 return mDataSwitchTileProvider.get();
+            case "compass":
+                return mCompassTileProvider.get();
         }
 
         // Custom tiles

16eec32eaf7f0c26450a234586617b47e5625ffb
Tim Zimmermann <tim@linux4.de>
2022-06-04 18:38:07 +0200

    core: Also spoof properties for samsung apps
    * Especially galaxy wearable apps don't work
      on samsung devices running AOSP
    
    Change-Id: I3663df8761c52ab8a1259c17e3a663ebf2b0ddbd

--- core/java/com/android/internal/gmscompat/AttestationHooks.java
+++ core/java/com/android/internal/gmscompat/AttestationHooks.java
@@ -28,6 +28,7 @@ import java.util.Arrays;
 public final class AttestationHooks {
     private static final String TAG = "GmsCompat/Attestation";
     private static final String PACKAGE_GMS = "com.google.android.gms";
+    private static final String SAMSUNG = "com.samsung.android.";
     private static final String FAKE_FINGERPRINT = "google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys";
 
     private static volatile boolean sIsGms = false;
@@ -75,7 +76,8 @@ public final class AttestationHooks {
     }
 
     public static void initApplicationBeforeOnCreate(Application app) {
-        if (PACKAGE_GMS.equals(app.getPackageName())) {
+        if (PACKAGE_GMS.equals(app.getPackageName())
+                || app.getPackageName().startsWith(SAMSUNG)) {
             sIsGms = true;
             spoofBuildGms();
         }
